17df9d8481bc796b8772f96feccab8711695bc4b
Rathna <rk.r1@tcs.com>
2020-07-03 19:47:02 +0530

    Karate Added

---
+++ KarateFirst/src/test/java/testRunner.java
@@ -0,0 +1,9 @@
+import org.junit.runner.RunWith;
+
+import com.intuit.karate.KarateOptions;
+
+@RunWith(com.intuit.karate.junit4.Karate.class)
+@KarateOptions(features = "classpath:API.feature")
+public class testRunner {
+
+}
---
+++ KarateFirst/target/test-classes/karate-config.js
@@ -0,0 +1,5 @@
+function fn() {
+	var config = {};
+	config.utils = {};
+	return config;
+}
